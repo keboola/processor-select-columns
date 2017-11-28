9517a4222df5bcf6d6dd213e1146ea3e972ac008
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-28 18:59:50 +0100

    fix: remove legacy script

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd $KBC_DATADIR/in/tables/
-find . -iname "*.csv" | xargs -n1 -I {} sh -c "/code/q -d , -H -O \"SELECT $KBC_PARAMETER_COLUMNS FROM {}\" > $KBC_DATADIR/out/tables/\"{}\""
